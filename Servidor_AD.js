4e3ea4f8557ed194951dbbc71caacbdb8c919232
Kolicks <ricardcasas@yahoo.es>
2022-07-14 22:00:11 +0200

    Funcional Basic

--- Servidor_AD.js
+++ Servidor_AD.js
@@ -1,27 +1,78 @@
 ///////////////////////////////////////////////////////////////////
 //                                                               //
 //                                                               //
-//                  API Gestió dels aparcaments                  //
-//                         Servidor UdG                          //
+//                        API Gràfica MQTT                       //
+//                                                               //
 //                                                               //
 ///////////////////////////////////////////////////////////////////
 
 
 const express = require("express");//Servidor HTTP
-const axios = require("axios").default;//Client HTTP per enviar al TTN
-const WebSocket = require('ws');
+const axios = require("axios").default;//Client HTTP per possible enviament al TTS
+const WebSocket = require('ws'); // WebSocket
+const mqtt = require('mqtt')// MQTT de recepció
+var options = { // Configuració i certificats del client MQTT
+    port: 1883,
+    clientId: 'mqttjs_' + Math.random().toString(16).substr(2, 8),
+    username: 'udg-practiques-ad@ttn',
+    password: 'NNSXS.5IJO52APVPDRLPAACRRBFRKXOG5KMUFLPLXXD6Q.3LIJ5GMMEO4TJSNS2Y6EOHVX5LIBFBAVQGVGMFX5DJFMMYXCKD7Q',
+    keepalive: 60,
+    reconnectPeriod: 1000,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3,
+    clean: true,
+    encoding: 'utf8'
+};
 
 
-//----------------------Executem el servidor i client--------------------------//
-const app = express();
-const port = 40300;
-const wss= new WebSocket.Server({host: "localhost",port: 40301});
-const ws = new WebSocket('ws://localhost:40301');
+//------------------------Executem el servidor i client WEB---------------------------------//
+const app = express(); // La seva execució
+const port = 40300; // Port de l'API Gràfica
+
+let portWS = 40301; // Port del WebSocket
+const wss= new WebSocket.Server({host: "localhost",port: portWS}); // Servidor WebSocket
+const ws = new WebSocket('ws://localhost:' + portWS);// Client WebSocket
 //Declara l'estructura de rebuda com a JSON
 app.use(express.json());
 
 
+//-------------------------------------- MQTT setup-----------------------------------------//
+
+// Client MQTT subscrit al Servidor MQTT de LoRaWAN
+const client  = mqtt.connect('https://eu1.cloud.thethings.network', options)
+
+// Poseu aquí l'ID del vostre dispositiu (End Device ID)
+// Quedareu subscrits ÚNICAMENT a aquest
+let DeviceID = 'eui-70b3d57ed005336c'; // 
+
+client.on('connect', function() {
+    console.log('Client connectat a TTN')
+    client.subscribe(`v3/udg-practiques-ad@ttn/devices/${DeviceID}/up`)
+});
+
+client.on('error', function(err) {
+    console.log(err);
+});
+
+client.on('message', function(topic, message) {
+    // La rebem i volquem a una variable objecte
+    var getDataFromTTN = JSON.parse(message);
+    console.log("Dades de TTN: ", getDataFromTTN.uplink_message.decoded_payload);
+    
+    /* Exemple que serveix per observar que podriem prescindir del Decoder de la API
+    var getFrmPayload = getDataFromTTN.uplink_message.frm_payload;
+    globalMQTT = Buffer.from(getFrmPayload, 'base64');
+    console.log(globalMQTT);*/
+    
+    //Utilitzem les ja descodificades i les enviem per WebSocket
+
+    ws.send(JSON.stringify(getDataFromTTN.uplink_message.decoded_payload));
+
+});
+
+
 //-----------------------Conneció del servidor WebSocketServer amb el Client---------------//
+
 wss.on('connection', function connection(ws){
     //console.log('Nova connexió' + ws.__socket.remoteAddress + ':' + ws.__socket.remotePort);
     ws.on('message', function incoming(message){
@@ -36,27 +87,15 @@ wss.on('connection', function connection(ws){
 });
 
 
-//------------------------------------ Pàgina Web ------------------------------------//
+//------------------------------------- Pàgina Web ----------------------------------------//
 
-app.get("/", (req, res) =>  {
+app.get("/", (req, res) =>  {// Responem a la petició del buscador web
     res.sendFile(`${__dirname}/Grafic.html`)
 });
 
-//------------------------------------ Rebuda POST de TTN ------------------------------------//
 
-app.post("/sensors", (req, res) => {
-    console.log('Dades del sensor rebudes');
+//------------------------------------- Rebuda Express ------------------------------------//
 
-    let obj = req.body.uplink_message.decoded_payload;
-
-    //console.log(obj);
-    ws.send(JSON.stringify(obj));
-
-    res.status(200).send();
-})
-
-
-//------------------------------------ Rebuda POST de TTN ------------------------------------//
 // Errors no descrits a l'aplicació
 app.use((error, req, res, next) => {
     res.status(500)
@@ -65,8 +104,7 @@ app.use((error, req, res, next) => {
     next(error)
 })
 
-
-  // Missatge d'avís, per la connexió del port
+// Missatge d'avís, per la connexió del port
 app.listen(port, () =>
     console.log(`Aplicació executada a la direcicó http://localhost:${port}`)
 );
